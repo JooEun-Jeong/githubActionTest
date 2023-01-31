a56eec6a5191f2b9e1a34f44daafb045cf1b489d
JooEun-Jeong <prichard147@naver.com>
2023-01-31 20:09:14 +0900

    fix date command argument error

--- count.sh
+++ count.sh
@@ -2,9 +2,9 @@
 
 # get current info
 DATE=$(date "+%Y.%m.%d")
-DAY=$(date + %d)
-MON=$(date + %0m)
-YEAR=$(date + %Y)
+DAY=$(date "+%d")
+MON=$(date "+%0m")
+YEAR=$(date "+%Y")
 TARGET_FILE_NAME="${MON}${DAY}"
 TARGET_FILE_PATH="JooEun/${TARGET_FILE_NAME}"
 COUNT=$(ls -l $TARGET_FILE_PATH | grep -E "(*.py|*.cpp|*.c)" | wc -l | sed 's/ //g')
--- updateReadme.sh
+++ updateReadme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get current info
-DATE=$(date + %Y.%m.%d)
-DAY=$(date + %d)
-MON=$(date + %0m)
-YEAR=$(date + %Y)
+DATE=$(date "+%Y.%m.%d")
+DAY=$(date "+%d")
+MON=$(date "+%0m")
+YEAR=$(date "+%Y")
 TARGET_FILE_NAME="${MON}${DAY}"
 TARGET_FILE_PATH="JooEun/${TARGET_FILE_NAME}"
 COUNT=$(ls -l $TARGET_FILE_PATH | grep -E "(*.py|*.cpp|*.c)" | wc -l | sed 's/ //g')
