5744e787686669e5bf9664eccb99e779d5684baa
JooEun-Jeong <prichard147@naver.com>
2023-02-06 21:09:38 +0900

    test

---
+++ alert.sh
@@ -0,0 +1,8 @@
+# get current info
+SLACK_WEBHOOK_URL=$1
+REPO_URL=$2
+
+DAILY_MESSAGE="Deploy 성공했습니다!\nRepo: ${REPO_URL}"
+echo $DAILY_MESSAGE
+# 슬랙 채널로 메시지 요청
+curl -X POST --data-urlencode "payload={\"channel\": \"#test-for-github-action\", \"username\": \"Alert Manager\", \"text\": \"${DAILY_MESSAGE}\", \"icon_emoji\": \":seal:\"}" $SLACK_WEBHOOK_URL
